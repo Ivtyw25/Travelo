c3e77b7cbacf1916ffcf3506d7e72b74d2794854
Mervyn0509 <mervynchew0095@gmail.com>
2024-08-04 07:08:53 +0800

    done foodies page

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,23 @@
-export default function Header(){
-    return(
-        <div class="p-4 flex justify-around items-center">
-            <h3 class="font-bold text-2xl">Travelo</h3>
-            <span class="font-medium">Home</span>
-            <span class="font-medium">Tours</span>
-            <button class="bg-slate-200 rounded-xl px-4 py-1 font-medium">Sign in</button>
+export default function Header() {
+    return (
+        <div className="p-4 flex justify-between items-center bg-black text-white">
+            <h3 className="font-bold text-3xl text-yellow-500 ml-20">Travelo</h3>
+            <div className="flex flex-grow justify-center">
+                <div className="flex space-x-32"> {/* Increased the spacing to space-x-32 */}
+                    <button className="bg-yellow-600 hover:bg-yellow-500 text-black rounded-xl px-4 py-2 font-medium transition duration-300">Home</button>
+                    <button className="bg-yellow-600 hover:bg-yellow-500 text-black rounded-xl px-4 py-2 font-medium transition duration-300">Tours</button>
+                </div>
+            </div>
+            <button className="bg-black text-white hover:bg-white hover:text-black rounded-xl px-4 py-2 font-medium transition duration-300 mr-4">Sign in</button>
         </div>
-    )
+    );
 }
+
+
+
+
+
+
+
+
 
