400bf73fe5c84be3dd2a4139965ddfb1b6909dd7
Yuan-0210 <houyuan0210@gmail.com>
2024-08-04 02:43:49 +0800

    Header modification

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,16 +1,19 @@
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" fill="currentColor"/>
-</svg>
 
-
-export default function Header(){
-    return(
-        <div class="p-4 flex justify-around bg-black text-white items-center">
-            <h3 class="font-bold text-2xl"><span class="pr-2">&#9992;</span>Travelo</h3>
-            <span class="font-medium">Home</span>
-            <span class="font-medium">Tours</span>
-            <button class="bg-gray-800 rounded-xl px-4 py-1 font-medium">Sign in</button>
+export default function Header() {
+    return (
+        <div className="navbar bg-black text-white flex justify-between items-center p-4">
+            <div className="leftBar flex items-center m-auto">
+                <h3 className="font-bold text-2xl"><span class="pr-2">&#9992;</span>Travelo</h3>
+            </div>
+            <div className="rightBar flex items-center gap-10 m-auto ">
+                <span className="font-medium hover:text-gray-400 cursor-pointer">Home</span>
+                <span className="font-medium hover:text-gray-400 cursor-pointer">Tours</span>
+                <button className="bg-gray-800 rounded-xl px-4 py-1 font-medium hover:bg-gray-700">
+                    Sign in
+                </button>
+            </div>
         </div>
-    )
+    );
 }
+
 
