89479be50d832759b348f15fa5905bfc178a366f
IvanTham <Ivantham789@gmail.com>
2024-08-04 03:35:43 +0800

    Add some styling

--- src/component/StateCard.jsx
+++ src/component/StateCard.jsx
@@ -1,11 +1,24 @@
-import './component.css'
-export default function StateCard(props){
+import './component.css';
+import { data } from '../data/state-mission';
 
+export default function StateCard(props) {
+  const { state } = props;
+  let matchCount = 0;
+  let stateCount = 0;
 
-    return(
-        <div className="containers">
-                <h1 className="title"> {props.state} </h1>
-                <h2 className="rate"> 3 / 10</h2>
-        </div>
-    )
+  data.forEach(item => { // Changed from map to forEach
+    if (item.State === state) {
+      if (item.status) {
+        matchCount += 1;
+      }
+      stateCount += 1;
+    }
+  });
+
+  return (
+    <div className="containers">
+      <h1 className="title">{state}</h1>
+      <h2 className="rate">{matchCount} / {stateCount}</h2>
+    </div>
+  );
 }
