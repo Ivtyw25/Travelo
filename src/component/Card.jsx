39b8f349525fa1e2b8d2fd34f3cc5aae5050ccea
IvanTham <Ivantham789@gmail.com>
2024-08-04 07:04:59 +0800

    linking main page with subpage

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({ src, alt, title, text }) => {
   return (
     <div className="card bg-gray-800 text-white p-4 shadow-lg rounded-lg w-36 flex-shrink-0 m-8">
       <img src={src} alt={alt} className="w-full h-36 object-cover rounded-t-lg" />
-      <h3 className="mt-3 text-center text-xl">{title}</h3>
+      <h3 className="mt-3 text-center">{title}</h3>
       <p className="mt-1 text-center">{text}</p>
     </div>
   );
